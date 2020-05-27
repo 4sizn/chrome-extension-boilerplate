d5ba63109afdf134c02a7b9b3ab80375db11a0e5
Hee Seok Shin <4sizn@naver.com>
2020-05-27 23:04:38 +0900

    chore : eslint

--- src/popup/index.tsx
+++ src/popup/index.tsx
@@ -2,6 +2,6 @@ import React from "react";
 import { render } from "react-dom";
 import { Popup } from "./Popup";
 
-chrome.tabs.query({ active: true, currentWindow: true }, tab => {
+chrome.tabs.query({ active: true, currentWindow: true }, (tab) => {
     render(<Popup />, document.getElementById("popup"));
 });
